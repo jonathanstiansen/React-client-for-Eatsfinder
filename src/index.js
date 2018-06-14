253163fc8b28d6c592a14f4ac0911d22b47a2aeb
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2018-06-13 20:02:27 -0700

    Create project EatsFinder react

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<p>New Project!</p>, document.getElementById("root"));
+registerServiceWorker();

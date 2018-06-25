95e5b5aa0b2014c6c1b53109f142cc38b89acd29
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2018-06-24 21:17:42 -0700

    add user request

---
+++ src/requests/user.js
@@ -0,0 +1,23 @@
+import { BASE_URL } from "../config";
+
+export default {
+  one(id) {
+    return fetch(`${BASE_URL}/users/${id}`, {
+      headers: { "Content-Type": "application/json" }
+    }).then(response => response.json());
+  },
+  create(params) {
+    return fetch(`${BASE_URL}/users`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params)
+    }).then(response => response.json());
+  },
+  update(id, params) {
+    return fetch(`${BASE_URL}/users/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params)
+    }).then(response => response.json());
+  }
+};
